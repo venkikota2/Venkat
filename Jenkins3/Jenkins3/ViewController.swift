c2131bcf8d89bb9550eaaa9ab2a87287ac2f33ad
KOTA VENKATESWARLU <kota.venkateswarlu@digital.datamatics.com>
2021-02-17 11:52:55 +0530

    Some changes

--- Jenkins3/Jenkins3/ViewController.swift
+++ Jenkins3/Jenkins3/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var btn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
